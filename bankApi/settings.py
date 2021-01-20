51ebd24e6b22ec2827fb8202f35587f438021246
Aayush <aayush.bcode@gmail.com>
2021-01-20 17:45:48 +0530

    testing

--- bankApi/settings.py
+++ bankApi/settings.py
@@ -143,10 +143,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ORIGIN_ALLOW_ALL = True
