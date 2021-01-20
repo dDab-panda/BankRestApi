b7f7e5c2830472a598863ef3115511c125173adf
Aayush <aayush.bcode@gmail.com>
2021-01-20 23:32:53 +0530

    testing at 11:30pm

--- bankApi/settings.py
+++ bankApi/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../app/staticfiles')
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
